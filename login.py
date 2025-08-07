c08015f2bd479865ac9c5c8c31098f968d6d65d3
Njien <thomasjessyendi@gmail.com>
2025-08-07 22:04:12 +0100

    update version of my assignment

---
+++ login.py
@@ -0,0 +1,55 @@
+verification_fee = 1500
+
+print("""
+		available coomands
+		"Login" to login account
+		"register" to create new account""")
+users_db = [{"username": "jessy", "password": "pass", "balance": 2000, "is_verified": False}]
+command = str(input("Enter login or register to continue:\n>>> ").lower())
+
+if command == "register":
+	username = str(input("create username\n>>> ").lower())
+	if username in users_db[0].values():
+		print(f"username '{username}' already exist\nEnter another username")
+	else:
+		password = str(input("Create password:\n>>> ").lower())
+		if password in users_db[0].values():
+			print(f"password '{password}' already exist\ncan not duplicate password")
+		else:
+			balance = float(input("Enter balance:\n>>> "))
+			is_verified = False
+
+			yes_no = str(input("Do you want get verified? yes/no;\n>>> ").lower())
+			if yes_no == "yes":
+				if balance >= verification_fee:
+					print("Verification update successful")
+					balance = balance - verification_fee
+					is_verified = True
+					users_db.append({"username": username, "password": password, "balance": balance, "is_verified": is_verified})
+					print(f" Registered Successful: {users_db[1]}")
+				else:
+					print(f"inssufficient fund: verification fee is: {verification_fee}, but your balance is {balance}")
+					
+					users_db.append({"username": username, "password": password, "balance": balance, "is_verified": is_verified})
+					print(f" Registered Successful: {users_db[1]}")
+			elif yes_no == "no":
+				users_db.append({"username": username, "password": password, "balance": balance, "is_verified": is_verified})
+				print(f" Registered Successful: {users_db[1]}")
+			else:
+				print(f"{yes_no} is an in valid command")
+elif command == "login":
+	username = str(input("Enter username\n>>> ").lower())
+	if username in users_db[0].values():
+		password = str(input("Enter password:\n>>> ").lower())
+		if password in users_db[0].values():
+			print(f"Login successful! Welcome back '{username}'")
+			print(f" users details: {users_db[0]}")
+		else:
+			print(f"password{password} mismatch for {username}")
+
+	else:
+		print(f"'{username}' account does not exist\nRegister new account")
+
+else:
+	print(f"invalid command!: '{command}' command does not exist")
+					
